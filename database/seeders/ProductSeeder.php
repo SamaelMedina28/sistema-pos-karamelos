2a97982fe0d8f91ad9cfa00d82f9bf8390ae291e
Samael Home <samaelortiz2218@gmail.com>
2025-12-17 15:37:56 -0800

    feat(products): implementar seeders

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
 class ProductSeeder extends Seeder
 {
@@ -12,6 +12,21 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Product::create([
+            'name' => 'Karamelos',
+            'image_path' => 'karamelos.jpg',
+            'price_for_kg' => 50,
+            'stock_quantity' => 10,
+        ], [
+            'name' => 'Chicles',
+            'image_path' => 'chicles.jpg',
+            'price_for_kg' => 40,
+            'stock_quantity' => 3,
+        ], [
+            'name' => 'Chicles de menta',
+            'image_path' => 'chicles_menta.jpg',
+            'price_for_kg' => 35,
+            'stock_quantity' => 3,
+        ]);
     }
 }
