c115dfff95716ace85ca7bce809694df4c966e02
Samael Home <samaelortiz2218@gmail.com>
2025-12-17 16:06:43 -0800

    feat: Add `StoreProductRequest` for product creation validation.

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -11,7 +11,7 @@ class StoreProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class StoreProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'image_path' => 'required|string|max:255',
+            'price_for_kg' => 'required|numeric',
+            'stock_quantity' => 'required|numeric',
         ];
     }
 }
