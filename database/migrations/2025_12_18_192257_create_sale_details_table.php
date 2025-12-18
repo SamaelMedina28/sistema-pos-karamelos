849dd412b54d61502c03987779a552e49e58d74f
Samael Home <samaelortiz2218@gmail.com>
2025-12-18 11:34:28 -0800

    feat: create `sale_details` table migration with sale, product, grams, and amount fields.

--- database/migrations/2025_12_18_192257_create_sale_details_table.php
+++ database/migrations/2025_12_18_192257_create_sale_details_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('sale_details', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('sale_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            // Cuantos gramos vendimos de este producto
+            $table->integer('grams');
+            // Cantidad pagada por el cliente por esa cantidad de gramos
+            $table->decimal('amount', 10, 2);
             $table->timestamps();
         });
     }
