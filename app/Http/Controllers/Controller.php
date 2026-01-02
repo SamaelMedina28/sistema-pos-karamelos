8c51294cacb0682469fb0344856bf78665a10e5a
Samael Home <samaelortiz2218@gmail.com>
2026-01-01 19:54:32 -0800

    feat: agregar anotaciones en controlador

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,25 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      x={
+ *          "logo": {
+ *              "url": "https://via.placeholder.com/190x90.png?text=SAAS 'Karamelos'"
+ *          }
+ *      },
+ *      title="SAAS 'Karamelos'",
+ *      description="Sistema de punto de venta para local de 'Karamelos'",
+ *      @OA\Contact(
+ *          email="samaelortiz2218@gmail.com"
+ *      ),
+ *     @OA\License(
+ *         name="MIT",
+ *         url="https://opensource.org/licenses/MIT"
+ *     )
+ * )
+ */
 abstract class Controller
 {
     //
